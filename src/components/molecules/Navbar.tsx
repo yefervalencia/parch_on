5767b184e7950d44ff11b17d4624cede479aa6d6
isay83 <jisay.2003@gmail.com>
2024-09-16 15:59:22 -0500

    add atomic design

---
+++ src/components/molecules/Navbar.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Image src="/ParchOn.png" alt="Logo" width={60} height={60} />
+      </div>
+      <ul className="navLinks">
+        <li>
+          <Link href="./home">Inicio</Link>
+        </li>
+        <li>
+          <Link href="./about">Nosotros</Link>
+        </li>
+        <li>
+          <Link href="./contact">Contacto</Link>
+        </li>
+        <li>
+          <Link href="./testimonies">Testimonio</Link>
+        </li>
+        <li>
+          <Link href="./events">Eventos</Link>
+        </li>
+      </ul>
+      <div className="authButtons">
+        <Link href="/login">
+          <button className="loginBtn">Iniciar Sesión</button>
+        </Link>
+        <Link href="/register">
+          <button className="registerBtn">Registrarme</button>
+        </Link>
+      </div>
+    </nav>
+  );
+};
